5262ba519da8793a949762ea57de0d796b51242a
jgoerzen <jpgarch@complete.org>
2002-11-21 09:40:27 -0600

    /grunt/head: changeset 65
    Initial version
    
    darcs-hash:20021121154027-a5eff-684bad59e79ad9487cd2a077c53a0f5028178d3d.gz

--- gruntreceive.py
+++ gruntreceive.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python2.2
 
-import sys, re, os, pwd
+import sys, re, os, pwd, time
 from sys import stdin, stdout
 import pyme, pyme.core
+import GnuPGInterface
 
 """File format:
 
@@ -48,3 +49,92 @@ if pwd.getpwuid(os.getuid())[0] != username:
     os.setregid(destgid)
     os.setreuid(destuid)
     
+assert pwd.getpwuid(os.getuid())[0] == username,\
+       "Could not change to correct user"
+
+HOMEDIR = pwd.getpwuid(os.getuid())[5] + '/.grunt'
+WORKDIR = HOMEDIR + '/work'
+assert os.path.isfile(HOMEDIR + '/validsigs.txt'),\
+       "File %s does not exist" % HOMEDIR + '/validsigs.txt'
+
+if not os.path.isdir(WORKDIR):
+    os.mkdir(WORKDIR, 0600)
+
+tmpfilename = WORKDIR + '/workfile-%d-%s' % (os.getpid(), time.time())
+gnupg = GnuPGInterface.GnuPG()
+process = gnupg.run(['--decrypt', '--output', tmpfilename],
+                    create_fhs=['status'],
+                    attach_fhs={'stdin': stdin})
+goodsig = None
+signatory = None
+while 1:
+    status = process.handles['status'].readline()
+    if len(status) == 0:
+        break
+    statuswords = status.strip().split(' ')
+    if statuswords[1] == 'GOODSIG':
+        goodsig = 1
+        signatory = statuswords[2]
+    if statuswords[1] == 'BADSIG':
+        goodsig = 0
+        
+if not goodsig:
+    try:
+        os.unlink(workfile)
+    except OSError:
+        pass
+    raise ValueError, "FAILURE: no signature detected!"
+
+# Now, check to see if this signature is in the user's list of acceptable ones.
+
+shortsignatory = signatory[-8:]
+foundvalid = 0
+
+gsf = open(HOMEDIR + '/validsigs.txt')
+for line in gsf.readlines():
+    if line.strip() == shortsignatory or line.strip() == signatory:
+        foundvalid = 1
+        break
+gsf.close()
+
+if not foundvalid:
+    try:
+        os.unlink(workfile)
+    except OSError:
+        pass
+    raise ValueError, "FAILURE: Key %s (%s) not found in list of valid keys."%\
+          (signatory, shortsignatory)
+
+# Good sig.  Process the data.
+
+datafile = open(tmpfilename)
+
+try:
+    assert datafile.readline(1024).strip() == ':GRUN:SECUREHEADER:FORMAT-1S:'
+    susername = re.search('^:USER:(.+)$', datafile.readline(1024).strip()).group(1)
+    susername = base64.decodestring(susername)
+    assert susername == username, 'FAILURE: secure packet username %s differs from regular username %s' % (susername, username)
+    
+    mode = re.search('^:MODE:(.+)$', datafile.readline(1024).strip()).group(1)
+    mode = base64.decodestring(mode)
+    
+    dest = re.search('^:DEST:(.+)$', datafile.readline(1024).strip()).group(1)
+    dest = base64.decodestring(dest)
+
+    assert datafile.readline(1024).strip() == ':DATA:'
+
+    if mode == 'EXEC':
+        outputfd = os.popen(dest, 'w')
+    elif mode == 'PUT':
+        outputfd = open(dest + '.gruntput', 'wb')
+    while 1:
+        data = datafile.read(10240)
+        if not len(data):
+            break
+        outputfd.write(data)
+    outputfd.close()
+    if mode == 'PUT':
+        os.rename(dest + '.gruntput', dest)
+finally:
+    datafile.close()
+    os.unlink(tmpfilename)
