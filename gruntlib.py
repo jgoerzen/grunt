994ad3bf132cfcebfb2678475008fa54895d54a0
jgoerzen <jpgarch@complete.org>
2002-11-22 15:52:39 -0600

    /grunt/head: changeset 98
    Fixed a conflict
    
    darcs-hash:20021122215239-a5eff-43b0744fce11219e45497e48f75e5f704f86d178.gz

--- gruntlib.py
+++ gruntlib.py
@@ -17,6 +17,7 @@
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import binascii, os, pwd, time, md5, fcntl, re
+from ConfigParser import ConfigParser
 
 def encode(binary):
     return binascii.b2a_base64(binary).strip()
