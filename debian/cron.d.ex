d627c2dde5cdd7a7075a05641791cbbbd1a9f291
jgoerzen <jpgarch@complete.org>
2002-12-13 10:41:18 -0600

    /grunt/head: changeset 102
    Various updates to prepare it for release.
    
    darcs-hash:20021213164118-a5eff-8639eb0603eafe11e78f56b59473a649587f03a4.gz

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the grunt package
-#
-0 4	* * *	root	grunt_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(grunt):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="grunt" command="/usr/bin/grunt"
